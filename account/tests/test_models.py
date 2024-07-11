b9543b904b4697f2b1d1511dbba7d25431dd0bbf
lucascody <lucassantiagocody@gmail.com>
2024-07-11 11:11:43 +0100

    Update test_models.py from account app tests

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -4,27 +4,25 @@ from account.models import CustomUser
 class CustomUserTestCase(TestCase):
     
     def setUp(self):
-        self.user_with_cpf = CustomUser(
+        self.user_with_cpf = CustomUser.objects.create_user(
             email='usercpf@gmail.com',
-            full_name='testfullname',
-            cpf_cnpj='333.333.333-33',
-            password='123'
+            password='123',
+            cpf_cnpj='333.333.333-33'
         )
         self.user_with_cpf.save()
 
-        self.user_with_cnpj = CustomUser(
+        self.user_with_cnpj = CustomUser.objects.create_user(
             email='usercnpj@gmail.com',
-            full_name='testfullname',
-            cpf_cnpj='22.222.222/2222-22',
-            password='123'
+            password='123',
+            cpf_cnpj='22.222.222/2222-22'
         )
         self.user_with_cnpj.save()
 
-    def test_check_user_with_cpf_cpf(self):
+    def test_check_user_with_cpf(self):
         user_cpf = self.user_with_cpf.cpf_cnpj
         self.assertEqual(user_cpf, '33333333333')
     
-    def test_check_user_with_cnpj_cnpj(self):
+    def test_check_user_with_cnpj(self):
         user_cnpj = self.user_with_cnpj.cpf_cnpj
         self.assertEqual(user_cnpj, '22222222222222')
 
@@ -36,4 +34,7 @@ class CustomUserTestCase(TestCase):
         formated_cnpj = self.user_with_cnpj.get_formated_cpf_cnpj()
         self.assertEqual(formated_cnpj, '22.222.222/2222-22')
     
-    
+    def test_check_password_user_with_cpf(self):
+        self.assertTrue(self.user_with_cpf.check_password('123'))
+    def test_check_password_user_with_cnpj(self):
+        self.assertTrue(self.user_with_cnpj.check_password('123'))
