7a2d60555ea1d5c787a490c23371ac2894b8209e
lucascody <lucassantiagocody@gmail.com>
2024-07-12 17:44:54 +0100

    Create register_user_request_validator

--- account/utils.py
+++ account/utils.py
@@ -1,6 +1,37 @@
 from .models import CustomUser
 from pycpfcnpj import cpfcnpj
+from ninja.errors import HttpError
 import re
+
+
+def register_user_request_validator(request_data):
+
+    check_cpf_cnpj = cpf_cnpj_validator(request_data.user.cpf_cnpj)
+    check_email = email_validator(request_data.user.email)
+    check_full_name = full_name_validator(request_data.user.full_name)
+    check_password = password_validator(request_data.user.password)
+    check_type_user = type_user_validator(request_data.type_user)
+    
+    messages_error = []
+
+    if not check_cpf_cnpj:
+        messages_error.append('Invalid cpf/cnpj')
+    if not check_email:
+        messages_error.append('Invalid E-mail')
+    if not check_full_name:
+        messages_error.append('Invalid full_name')
+    if not check_type_user:
+        messages_error.append('Invalid type_user. Types user allowed are (commun, storekeeper)')
+    if not check_password:
+        messages_error.append('Invalid password min 6 characters')
+
+    single_msg = error_message_handler(messages_error)
+
+    if single_msg:
+        raise HttpError(status_code=400, message=single_msg)
+
+    return True
+    
 
 def cpf_cnpj_validator(cpf_cnpj_value):
     if cpfcnpj.validate(cpf_cnpj_value):
