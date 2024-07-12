a665bdaac0bf593b4fd1960dc30dc566c98dca8c
lucascody <lucassantiagocody@gmail.com>
2024-07-12 13:08:16 +0100

    Add django secret key value and allowed hosts value in .env file

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h99#5*vp-g4a^mxdvqc&9ecbo!7k=d@ze9=q*5e!rc-jgk%1%j'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host for host in config('ALLOWED_HOSTS').split(',')]
 
 
 # Application definition
