a3d651d696970ac0bc49dd899a126d840c911842
lucascody <lucassantiagocody@gmail.com>
2024-07-12 17:41:30 +0100

    Create error_message_handler

--- account/utils.py
+++ account/utils.py
@@ -32,3 +32,21 @@ def type_user_validator(type_user):
     if type_user and type_user in types_user_allowed:
         return True
     return False
+
+
+def error_message_handler(messages_error):
+    single_message = ""
+
+    for i, msg_error in enumerate(messages_error):
+        single_message += f'{msg_error}, '
+
+    single_message = single_message.strip()
+    # to dont display the last comma
+    single_message = single_message[:len(single_message)-1]
+
+    return single_message
+
+
+
+
+
