4bf0425cb165d2f9dae1ec26cb82e3bcae479ab8
lucascody <lucassantiagocody@gmail.com>
2024-07-12 02:27:58 +0100

    Create email_validator function validator

--- account/utils.py
+++ account/utils.py
@@ -1,6 +1,17 @@
+from .models import CustomUser
 from pycpfcnpj import cpfcnpj
+import re
 
 def cpf_cnpj_validator(cpf_cnpj_value):
     if cpfcnpj.validate(cpf_cnpj_value):
         return True
+    return False
+
+def email_validator(email):
+    email_regex_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+    if re.fullmatch(email_regex_pattern, email):
+        check_email = CustomUser.objects.filter(email=email)
+        if not check_email:
+            return True
     return False
