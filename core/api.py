7f76c53534e665355522d0adfc6f706c5591d3e2
lucascody <lucassantiagocody@gmail.com>
2024-07-13 01:02:17 +0100

    Create endpoint for payment app

--- core/api.py
+++ core/api.py
@@ -1,6 +1,9 @@
 from ninja import NinjaAPI
 from user.api import router as users_router
+from payment.api import router as payments_router
+
 
 api = NinjaAPI()
 
 api.add_router('user/', users_router)
+api.add_router('payment/', payments_router)
