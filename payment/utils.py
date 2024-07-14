0f8124f20a466eef15d2c9b04c67abacf7c2147b
lucascody <lucassantiagocody@gmail.com>
2024-07-15 00:32:45 +0100

    Create payer_payee_validator function validator

--- payment/utils.py
+++ payment/utils.py
@@ -3,6 +3,14 @@ from user.models import CustomUser
 from core.roles import Commun
 from decimal import Decimal
 
+def payer_payee_validator(payer:int, payee:int):
+    check_payer = CustomUser.objects.filter(id=int(payer))
+    check_payee = CustomUser.objects.filter(id=int(payer))
+
+    if check_payer and check_payee:
+        return True
+    return False
+      
 def amount_validator(amount):
     if isinstance(amount, Decimal):
         return True
