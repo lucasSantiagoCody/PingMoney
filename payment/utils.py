8485f232a525a4a0ab97f7ff8e3dc5459c1c1887
lucascody <lucassantiagocody@gmail.com>
2024-07-15 00:33:53 +0100

    Create transfer_request_validator function validator

--- payment/utils.py
+++ payment/utils.py
@@ -1,7 +1,33 @@
+from core.utils import error_message_handler
 from rolepermissions.checkers import has_role
 from user.models import CustomUser
+from ninja.errors import HttpError
 from core.roles import Commun
 from decimal import Decimal
+
+def transfer_request_validator(request_data):
+    check_payer_payee = payer_payee_validator(
+            request_data.payer, 
+            request_data.payee
+            )
+    payer_has_permission = check_payer_has_permission(request_data.payer)
+    check_amount = amount_validator(request_data.value)
+    
+    messages_error = []
+
+    if not check_payer_payee:
+        messages_error.append("Payer or Payee isn't registered")
+    if not check_amount:
+        messages_error.append('Amount must be Decimal')
+
+    if payer_has_permission == False:
+        messages_error.append("Storekeepers can't make transfer")
+
+    single_message_error = error_message_handler(messages_error)
+
+    if single_message_error:
+        raise HttpError(status_code=400, message=single_message_error)
+    return True
 
 def payer_payee_validator(payer:int, payee:int):
     check_payer = CustomUser.objects.filter(id=int(payer))
