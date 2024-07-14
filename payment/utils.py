616a673493ff6273ab0c7ef1b07d781ce12ca927
lucascody <lucassantiagocody@gmail.com>
2024-07-15 00:27:52 +0100

    Create amount_validator function validator

--- payment/utils.py
+++ payment/utils.py
@@ -1,6 +1,12 @@
 from rolepermissions.checkers import has_role
 from user.models import CustomUser
 from core.roles import Commun
+from decimal import Decimal
+
+def amount_validator(amount):
+    if isinstance(amount, Decimal):
+        return True
+    return False
 
 def check_payer_has_permission(payer_id):
     """ 
