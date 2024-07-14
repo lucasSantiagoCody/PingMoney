7052b35e2a381b6608260cd9195a5dbc34b68d2d
lucascody <lucassantiagocody@gmail.com>
2024-07-15 00:26:41 +0100

    Create check_payer_has_permission function

---
+++ payment/utils.py
@@ -0,0 +1,20 @@
+from rolepermissions.checkers import has_role
+from user.models import CustomUser
+from core.roles import Commun
+
+def check_payer_has_permission(payer_id):
+    """ 
+        if return False
+            - Payer doesn't has permission
+        if return None
+            - User isn't registerd
+    """
+
+    try:
+        payer = CustomUser.objects.get(id=payer_id)
+    except:
+        return None
+    
+    has_permission = has_role(payer, Commun)
+    if not has_permission:
+       return False
