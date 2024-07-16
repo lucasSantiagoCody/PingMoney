e2ae3deebc983d923a8bd7092001f20d9cdfdc61
lucascody <lucassantiagocody@gmail.com>
2024-07-16 01:46:24 +0100

    Create deposit api view

--- payment/api.py
+++ payment/api.py
@@ -1,5 +1,5 @@
-from .utils import transfer_request_validator
-from .schemas import TransferSchema
+from .utils import transfer_request_validator, deposit_request_validator
+from .schemas import TransferSchema, DepositSchema
 from ninja.errors import HttpError
 from user.models import CustomUser
 from ninja.router import Router
@@ -7,6 +7,8 @@ from .models import Transaction
 import requests
 
 router = Router()
+
+AUTHORIZER_ENDPOINT = "https://util.devi.tools/api/v2/authorize"
 
 @router.post('transfer/', response={200: dict, 401: dict, 500: dict})
 def transfer(request, request_data:TransferSchema):
@@ -17,8 +19,6 @@ def transfer(request, request_data:TransferSchema):
     payer = CustomUser.objects.get(id=request_data.payer)
     payee = CustomUser.objects.get(id=request_data.payee)
     amount_to_transfer = request_data.value
-
-    AUTHORIZER_ENDPOINT = "https://util.devi.tools/api/v2/authorize"
 
     authorizer_response = (requests.get(url=AUTHORIZER_ENDPOINT)).json()
 
@@ -54,5 +54,39 @@ def transfer(request, request_data:TransferSchema):
         except:
             transaction_fail()
         return {'message': 'transfer done successfully'}
+    else:
+        raise HttpError(status_code=401, message='Unauthorized transfer')
+
+@router.post('deposit/', response={200: dict})
+def deposit(request, request_data:DepositSchema):
+
+    deposit_request_validator(request_data)
+
+    depositor_id = request_data.depositor
+    amount = request_data.amount
+    depositor = CustomUser.objects.get(id=depositor_id)
+
+    def deposit_fail():
+        raise HttpError(status_code=500, message='Deposit fail, try again')
+
+    authorizer_response = (requests.get(url=AUTHORIZER_ENDPOINT)).json()
+
+    if authorizer_response['data']['authorization'] == True:
+        try:
+            depositor.receive(amount)
+            depositor.save()
+            return {'message': 'Deposit done successfully'}
+        except:
+            deposit_fail()
+        try:
+            transaction = Transaction(
+                payer=depositor,
+                payee=depositor,
+                amount=amount,
+                transaction_type='d'
+            )
+            transaction.save()
+        except:
+            deposit_fail()
     else:
         raise HttpError(status_code=401, message='Unauthorized transfer')
